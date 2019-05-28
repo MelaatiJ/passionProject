a3b88486f5bf80a95b372ef25100ccde42b569e5
MelaatiJ <fathimajayah876@gmail.com>
2019-05-28 14:29:50 -0500

    completed end routes for M&M site

--- passionProject/passionApp/urls.py
+++ passionProject/passionApp/urls.py
@@ -12,20 +12,35 @@ urlpatterns = [
 
     path('addArticle/', views.addArticle, name='addArticle'),
     path('viewAllArticles/', views.viewAllArticles, name='viewArticles'),
+    path('editArticle/', views.editArticle, name='editArticle'),
+    path('deleteArticle/', views.deleteArticle, name='deleteArticle'),
 
     path('addDiscussionPost/', views.addDiscussionPost, name='addDiscussionPost'),
     path('viewAllDiscussionPost/', views.viewAllDiscussionPost, name='viewAllDiscussionPost'),
+    path('editDiscussion/', views.editDiscussion, name='editDiscussion'),
+    path('deleteDiscussion/', views.deleteDiscussion, name='deleteDiscussion'),
 
 
-
+    # events page
     path('addUpcomingEvent/', views.addUpcomingEvent, name='addUpcomingEvent'),
+    path('editUpcomingEvent/', views.editUpcomingEvent, name='editUpcomingEvent'),
+    path('deleteUpcomingEvent/', views.deleteUpcomingEvent, name='deleteUpcomingEvent'),
     path('viewAllUpcomingEvents/', views.viewAllUpcomingEvents, name='viewAllUpcomingEvents'),
+    path('listAllPastEvents/', views.listAllPastEvents, name='listAllPastEvents'),
 
-    path('addBook', views.addBook, name='addBook'),
-    path('listBooks', views.listBooks, name='listBooks'),
+    # library page
+    path('addBook/', views.addBook, name='addBook'),
+    path('editBook/', views.editBook, name='editBook'),
+    path('deleteBook', views.deleteBook, name='deleteBook'),
+    path('listBooks/', views.listBooks, name='listBooks'),
 
-    path('addPhoto', views.addPhoto, name='addPhoto'),
-    path('listPhotos', views.listPhotos, name='listPhotos'),
+    # Gallery Page
+    path('addPhoto/', views.addPhoto, name='addPhoto'),
+    path('editPhoto/', views.editPhoto, name='editPhoto'),
+    path('deletePhoto/', views.deletePhoto, name='deletePhoto'),
+    path('listPhotos/', views.listPhotos, name='listPhotos'),
+
+
 
 
 
