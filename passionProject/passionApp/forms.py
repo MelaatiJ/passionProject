6f5136ce04d569f826b712534a8b550fb60d07be
MelaatiJ <fathimajayah876@gmail.com>
2019-05-30 10:34:54 -0500

    forms are complete and now working in my views to get the form on my webpage. Goal by 3 is to have all the forms on the pages. There are 7 forms.

--- passionProject/passionApp/forms.py
+++ passionProject/passionApp/forms.py
@@ -1,1 +1,47 @@
 from django import forms
+from .models import NaniEntryModel, DiscussionEntryModel, CommentEntryModel, GalleryEntryModel, \
+    UpcomingEventsEntryModel, LibraryEntryModel, MembershipEntryModel
+
+
+# for for post only created by admin or VP
+class NaniForm(forms.ModelForm):
+    class Meta:
+        model = NaniEntryModel
+        fields = "__all__"
+
+
+
+class DiscussionForm(forms.ModelForm):
+    class Meta:
+        model = DiscussionEntryModel
+        fields = "__all__"
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CommentEntryModel
+        fields="__all__"
+
+
+class GalleryForm(forms.ModelForm):
+    class Meta:
+        model =GalleryEntryModel
+        fields="__all__"
+
+class UpcomingEventForm(forms.ModelForm):
+    class Meta:
+        model = UpcomingEventsEntryModel
+        fields = "__all__"
+
+
+class LibraryForm(forms.ModelForm):
+    class Meta:
+        model = LibraryEntryModel
+        fields="__all__"
+
+
+class MembershipForm(forms.ModelForm):
+    class Meta:
+        model = MembershipEntryModel
+        fields = "__all__"
+
--- passionProject/passionApp/views.py
+++ passionProject/passionApp/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from .models import NaniEntryModel, DiscussionEntryModel, GalleryEntryModel, CommentEntryModel, LibraryEntryModel, \
+    MembershipEntryModel
+from .forms import NaniForm, DiscussionForm, CommentForm, GalleryForm, UpcomingEventForm, LibraryForm, MembershipForm
+
+
 # Create your views here.
 
 
@@ -7,47 +12,46 @@ def index(request):
     return HttpResponse("Testing server")
 
 
-#  Member page
-
-def newMember(request):
-    return HttpResponse("New member Form ")
-
-
-def profile(request):
-    return HttpResponse("Profile Page")
-
-
-def allMembers(request):
-    return HttpResponse("Be able to list all members of the club")
-
 # Front page where all the articles Athena and I Post that are informative regarding the whole Asian World
 
 
 def addNani(request):
-    return HttpResponse("Be able to add article only if you are admin or athenaVP")
+    form = NaniForm(request.POST or None, request.FILES or None)
+    context = {
+        "form": form
+    }
+    if request.method == "POST":
+        NaniEntryModel.objects.create(topic=request.POST["topic"], date=request.POST["date"],
+                                      image=request.POST["image"],
+                                      entry=request.POST["entry"], video=request.POST["video"], URL_link=["URL_link"])
+        return redirect("index")
+
+    return render(request, "passionApp/addNani.html", context)
 
 
 def viewAllNani(request):
     return HttpResponse("Everyone can view the info the admins post weather you are a member or not")
 
 
-
 def editNani(request):
     return HttpResponse("Need to be able to edit any post made from the President and VP")
-
 
 
 def deleteNani(request):
     return HttpResponse("Need to be able to delete any post made from President and VP")
 
+
 # Discussion Post Page
 
 def addDiscussion(request):
+    form = DiscussionForm(request.POST or None, request.FILES or None)
+
     return HttpResponse("Only members and admin can make a post and you have to be logged in to add a post")
 
 
 def viewAllDiscussion(request):
     return HttpResponse("Only Members loggeed in can view discussion board")
+
 
 def editDiscussion(request):
     return HttpResponse("only if the user who created the post can edit it")
@@ -62,6 +66,7 @@ def deleteDiscussion(request):
 def addComment(request):
     return HttpResponse("Members are able to add a single comment on the discussion post.")
 
+
 # upcoming events
 
 
@@ -72,11 +77,14 @@ def addUpcomingEvent(request):
 def editUpcomingEvent(request):
     return HttpResponse("Only admins can edit post")
 
+
 def deleteUpcomingEvent(request):
     return HttpResponse("only admins can delete post")
 
+
 def viewAllUpcomingEvents(request):
     return HttpResponse("Everyone can view upcoming events whether your are logged in  or not")
+
 
 # past events
 
@@ -94,6 +102,7 @@ def addBook(request):
 def editBook(request):
     return HttpResponse("only admin and vp can edit book made ")
 
+
 def deleteBook(request):
     return HttpResponse("Only admin and VP can delete book made")
 
@@ -102,24 +111,34 @@ def listBooks(request):
     return HttpResponse("Everyone can view books in the library so they can see the genres and different themes, good "
                         "for advertising")
 
+
 # photo gallery
 
 def addPhoto(request):
     return HttpResponse("All members can add photo")
 
+
 def editPhoto(request):
     return HttpResponse("Whoever made post and ADMIN can edit photo")
 
+
 def deletePhoto(request):
     return HttpResponse("Only whoever made post and ADMIN can delete photo")
+
 
 def listPhotos(request):
     return HttpResponse("Only members and ADMIN can view photos")
 
 
+#  Member page ( not being implemented until everything else is complete )
+
+def newMember(request):
+    return HttpResponse("New member Form ")
 
 
+def profile(request):
+    return HttpResponse("Profile Page")
 
 
-
-
+def allMembers(request):
+    return HttpResponse("Be able to list all members of the club")
